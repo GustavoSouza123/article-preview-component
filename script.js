eacdac4e446c7c87ae259702eaf3de3598be232c
Gustavo Souza <gustavoelia7@gmail.com>
2023-12-08 08:51:31 -0300

    feats: share popup, responsiveness (finished)

---
+++ script.js
@@ -0,0 +1,23 @@
+let share = document.querySelector('.article-preview .info .bottom .share');
+let popup = document.querySelector('.article-preview .info .bottom .share .popup');
+let svg = document.querySelector('svg.share-svg path');
+
+let active = false;
+share.addEventListener('click', function() {
+    active = (active) ? false : true;
+    if(active) {
+        popup.style.display = 'flex';
+        share.style.backgroundColor = '#6d829a';
+        svg.setAttribute('fill', '#ffffff');
+        setTimeout(function() {
+            popup.style.opacity = '1';
+        }, 100);
+    } else {
+        popup.style.opacity = '0';
+        share.style.backgroundColor = '#edf2f8';
+        svg.setAttribute('fill', '#6d829a');
+        setTimeout(function() {
+            popup.style.display = 'none';
+        }, 200);
+    }
+})
